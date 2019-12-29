ff14748515582b1e95b8fa39968aa82e6fc094d7
Jordan <knightworks@pm.me>
2019-12-29 09:57:55 -0600

    updates

--- Field.svelte
+++ Field.svelte
@@ -5,11 +5,12 @@
     export let label = name.replace('_', ' ')
     export let placeholder = name
     export let errors = []
+    export let help
 </script>
 
-<div class="field">
-    <label>{label}</label>
-    <div class="control">
+<fieldset class="field">
+    <label for={name}>{label}</label>
+    <div class='control'>
         {#if type === 'text'}
             <input type='text' {name} bind:value={value} {placeholder}>
         {:else if type === 'password'}
@@ -21,46 +22,8 @@
         {/if}
     </div>
     {#if errors.length}
-        <div class="error">{errors[0]}</div>
+        <p class='error'>{errors[0]}</p>
+    {:else if help}
+        <p class='help'>{errors[0]}</p>
     {/if}
-</div>
-<style type="scss">
-.field {
-    margin-bottom: 1rem;
-    label {
-        text-transform: capitalize;
-        display: inline-block;
-        padding-top: calc(.375rem + 1px);
-        padding-bottom: calc(.375rem + 1px);
-        margin-bottom: 0;
-        font-size: inherit;
-        line-height: 1.5;
-    }
-    .control {
-        display: block;
-        width: 100%;
-        height: calc(1.5em + .75rem + 2px);
-        padding: .375rem .75rem;
-        font-size: .9rem;
-        font-weight: 400;
-        line-height: 1.5;
-        color: hsl(210, 8.8%, 31.4%);
-        background-color: hsl(0, 0%, 100%);
-        background-clip: padding-box;
-        border: 1px solid hsl(263.1, 18.8%, 86.5%);
-        border-radius: 5px;
-        box-sizing: border-box;
-    }
-    input {
-        background: transparent;
-        border: none;
-        width: 100%;
-        &::placeholder {
-            text-transform:capitalize;
-        }
-    }
-    .errors {
-
-    }
-}
-</style>
+</fieldset>
